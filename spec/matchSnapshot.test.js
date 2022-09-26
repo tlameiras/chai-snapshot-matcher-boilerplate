106b736389d2ffe8b6f8a4779c801a95b2789ccb
Tiago Lameiras <tiagoplameiras@gmail.com>
2022-09-26 11:49:50 +0100

    Fix tests that use the hint attribute

--- spec/matchSnapshot.test.js
+++ spec/matchSnapshot.test.js
@@ -1,32 +1,32 @@
 const { expect } = require('chai');
 
-describe('chai-snapshot-matcher', function() {
-  describe('- matchSnapshot -', function() {
-    it('use matchSnapshot to expect numbers', function() {
+describe('chai-snapshot-matcher', function () {
+  describe('- matchSnapshot -', function () {
+    it('use matchSnapshot to expect numbers', function () {
       const testVar = 1;
 
       expect(testVar).to.matchSnapshot(this);
     });
 
-    it('use matchSnapshot to expect booleans', function() {
+    it('use matchSnapshot to expect booleans', function () {
       const testVar = true;
 
       expect(testVar).to.matchSnapshot(this);
     });
 
-    it('use matchSnapshot to expect strings', function() {
+    it('use matchSnapshot to expect strings', function () {
       const testVar = 'Hello World!';
 
       expect(testVar).to.matchSnapshot(this);
     });
 
-    it('use matchSnapshot to expect arrays', function() {
+    it('use matchSnapshot to expect arrays', function () {
       const testVar = [1, 'two', '3'];
 
       expect(testVar).to.matchSnapshot(this);
     });
 
-    it('use matchSnapshot to expect objects', function() {
+    it('use matchSnapshot to expect objects', function () {
       const testVar = {
         a: {
           b: 1,
@@ -38,17 +38,17 @@ describe('chai-snapshot-matcher', function() {
       expect(testVar).to.matchSnapshot(this);
     });
 
-    it('use the hint attribute to customize the snapshot title', function() {
+    it('use the hint attribute to customize the snapshot title', function () {
       const testVar = 'custom name';
 
-      expect(testVar).to.matchSnapshot(this, '(go check the __snapshots__ folder)');
+      expect(testVar).to.matchSnapshot(this, { hint: '(go check the __snapshots__ folder)' });
     });
 
-    it('use the hint attribute to have more than one matchSnapshot per it', function() {
+    it('use the hint attribute to have more than one matchSnapshot per it', function () {
       const testVar = 10;
 
-      expect(testVar).to.matchSnapshot(this, '(value)');
-      expect(typeof testVar).to.matchSnapshot(this, '(type)');
+      expect(testVar).to.matchSnapshot(this, { hint: '(value)' });
+      expect(typeof testVar).to.matchSnapshot(this, { hint: '(type)' });
     });
   });
 });
